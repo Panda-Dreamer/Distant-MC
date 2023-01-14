0b4273daac57371a12f1bd1ba1cddb08ad3e514c
Omega <castoretpapillon@gmail.com>
2023-01-14 20:37:22 +0100

    eae

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 5000 });
 
 wss.on('connection', function connection(ws) {
+  console.log("Conenction ! ")
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
-
+  
   ws.send('something');
 });
