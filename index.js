401ffda1d1adf0bc74bf4c62e90368cdeea63239
Omega <castoretpapillon@gmail.com>
2023-01-16 22:41:46 +0100

    eae

--- index.js
+++ index.js
@@ -6,17 +6,65 @@ const config = require("./config.json")
 const express = require("express");
 const { createServer } = require("http");
 const { type } = require('os');
+const { channel } = require('diagnostics_channel');
 const app = express();
 const httpServer = createServer(app);
 app.use(express.json());
 
 
+
+
+
+sockets = {
+}
+
+drops = {
+
+}
+
 wss.on('connection', function connection(ws) {
   console.log("Connection ! ")
   ws.on('message', function message(data) {
-    channel = data.toString().split("[CHANNEL]")[0]
+    channel = data.toString().split("$$$")[0]
+    data = data.toString().split("$$$")[1]
     if(channel == "WEBHOOK"){
-      sendMessage(config.url,data.toString().split("[CHANNEL]")[1])
+      sendMessage(config.url,{content:data})
+    }else if(channel == "join"){
+      sockets[ws] = data
+      
+    }else if(channel == "item"){
+      if(drops[ws] == undefined){
+        drops[ws] = {}
+      }
+      if(drops[ws][data] == undefined){
+        drops[ws][data] = 0
+      }
+      drops[ws][data] += 1
+
+      sendMessage(config.url,{
+        "username": "Glacite farming",
+        "avatar_url": "",
+        "content": "",
+        "embeds": [
+          {
+            "title": "RARE DROP",
+            "color": 50421,
+            "description": `${sockets[ws]} just dropped ${data}\n\nTimes dropped during the session: ${drops[ws][data]} `,
+            "timestamp": "",
+            "author": {
+              "name": ""
+            },
+            "image": {},
+            "thumbnail": {},
+            "footer": {},
+            "fields": []
+          }
+        ],
+        "components": []
+      })
+    }else if(channel == "sessionStart"){
+      drops[ws] = {}
+      sendMessage(config.url,{content:`${sockets[ws]} just started a session, drops count reset !`})
     }
   });
   ws.send('something');
@@ -28,9 +76,7 @@ app.get("/", (req, res) => {
   
 async function sendMessage(webhookUrl, message) {
   try {
-      const response = await axios.post(webhookUrl, {
-          content: message
-      });
+      const response = await axios.post(webhookUrl, message);
       console.log(`Message sent successfully: ${response.status}`);
   } catch (error) {
       console.error(`Error sending message: ${error}`);
