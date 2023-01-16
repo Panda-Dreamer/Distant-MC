e7851297134c92fd7f34b649655b4c0601af283d
Omega <castoretpapillon@gmail.com>
2023-01-16 16:58:04 +0100

    ea

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 5001 });
-
+const axios = require("axios")
 
 
 const express = require("express");
@@ -15,7 +15,9 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
-  
+  ws.on('webhook', function message(data) {
+    sendMessage("https://discord.com/api/webhooks/1064275921462382642/O9KMso7-Pa9hWrYll8b6SfyNN5zqM9acCK5CBTacLow3xsfEXF5rAF5ZiQpIaLh4ZIFr",data)
+  });
   ws.send('something');
 });
 
@@ -23,4 +25,16 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/page.html");
 });
   
+async function sendMessage(webhookUrl, message) {
+  try {
+      const response = await axios.post(webhookUrl, {
+          content: message
+      });
+      console.log(`Message sent successfully: ${response.status}`);
+  } catch (error) {
+      console.error(`Error sending message: ${error}`);
+  }
+}
+
+
 httpServer.listen(5000);
